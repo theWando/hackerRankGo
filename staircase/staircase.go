0b93aa37866f9f96d6692f795b494f82071baeb7
Eduardo Laguna <eduardo.laguna@mercadolibre.cl>
2020-07-23 13:13:25 -0400

    adds staircase's problem url

--- staircase/staircase.go
+++ staircase/staircase.go
@@ -10,6 +10,7 @@ import (
 )
 
 // Complete the staircase function below.
+// https://www.hackerrank.com/challenges/staircase/problem?h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
 func staircase(n int32) {
 	fmt.Printf(makeStair(n))
 }
